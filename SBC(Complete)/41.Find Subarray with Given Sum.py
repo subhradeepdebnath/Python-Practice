af5e9475be1806ecc70d6911b36048f9bb3617c1
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-02 01:11:20 +0530

    DSA

---
+++ SBC(Complete)/41.Find Subarray with Given Sum.py
@@ -0,0 +1,13 @@
+def func(arr,target):
+    for i in range(len(arr)):
+        sum=0
+        for j in range(i,len(arr)):
+            sum+=arr[j]
+            if sum==target:
+                if sum==target:
+                    for k in range(i,j+1):
+                        print(arr[k], end=" ")
+                    return
+arr=list(map(int, input().split()))
+target=int(input())
+func(arr,target)
