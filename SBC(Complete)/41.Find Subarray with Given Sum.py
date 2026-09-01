3a4b9a7d15bf6608d6d08662213d8136600ddc45
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-02 01:13:30 +0530

    DSA

--- SBC(Complete)/41.Find Subarray with Given Sum.py
+++ SBC(Complete)/41.Find Subarray with Given Sum.py
@@ -3,7 +3,6 @@ def func(arr,target):
         sum=0
         for j in range(i,len(arr)):
             sum+=arr[j]
-            if sum==target:
                 if sum==target:
                     for k in range(i,j+1):
                         print(arr[k], end=" ")
