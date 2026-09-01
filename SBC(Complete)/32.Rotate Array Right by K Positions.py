9adea2c0b6809d749f5530dcc66a8374c617e353
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-01 21:49:20 +0530

    hello world code

--- SBC(Complete)/32.Rotate Array Right by K Positions.py
+++ SBC(Complete)/32.Rotate Array Right by K Positions.py
@@ -1,4 +1,17 @@
-def func(arr,k ):
-    for i in range()
-arr=list(map(int, input().split()))
-func(arr)
+def func(arr, k):
+
+    for i in range(k):
+
+        last = arr[-1]
+
+        for j in range(len(arr)-1, 0, -1):
+            arr[j] = arr[j-1]
+
+        arr[0] = last
+
+    print(*arr)
+
+arr = list(map(int, input().split()))
+k = int(input())
+
+func(arr, k)
