35f7a75801cd756b7f6d916a2597ac08dc73702e
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-02 00:35:08 +0530

    DSA

---
+++ SBC(Complete)/36.Find Pair with Given Sum.py
@@ -0,0 +1,9 @@
+def func(arr):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i]+arr[j]==target:
+                print(arr[i],arr[j])
+                break
+arr=list(map(int, input().split()))
+target=int(input())
+func(arr)
