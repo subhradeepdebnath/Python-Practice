140367c0b450ec711d34c4d9fec0a804f6d85add
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-02 01:43:54 +0530

    DSA

---
+++ SBC(Complete)/44.Find Longest Consecutive Sequence.py
@@ -0,0 +1,18 @@
+def func(arr):
+    arr.sort()
+    count=1
+    maximum=1
+
+    for i in range(len(arr)-1):
+        if arr[i+1] == arr[i]+1:
+            count+=1
+        else:
+            count=1
+
+        if count>maximum:
+            maximum=count
+
+    print(maximum)
+
+arr=list(map(int,input().split()))
+func(arr)
