6cae877de4d8f002d49429639f906a3a6eacf5b3
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-04 13:37:44 +0530

    DSA

---
+++ SBC(Complete)/67.Find First Non-Repeating Character.py
@@ -0,0 +1,11 @@
+def func(s):
+    for ch in s:
+        count=0
+        for i in s:
+            if ch==i:
+                count+=1
+        if count==1:
+            print(ch)
+            return
+s=input()
+func(s)
