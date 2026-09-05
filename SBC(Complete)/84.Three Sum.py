7cfac116f81212c08ed4106dd51f664d4d51c17f
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-05 15:49:57 +0530

    DSA

---
+++ SBC(Complete)/84.Three Sum.py
@@ -0,0 +1,10 @@
+def func(arr,target):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            for k in range(i+2, len(arr)):
+                if arr[i]+arr[j]+arr[k]==target:
+                    print(arr[i],arr[j],arr[k])
+                    return
+arr=list(map(int, input().split()))
+target=int(input())
+func(arr,target)
