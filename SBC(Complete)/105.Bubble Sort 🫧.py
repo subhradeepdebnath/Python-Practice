7d4ebdb9547a6a6a701eb9f7914346c735cb8c8e
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-06 14:17:52 +0530

    DSA

---
+++ SBC(Complete)/105.Bubble Sort 🫧.py
@@ -0,0 +1,8 @@
+def func(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+    print(*arr)
+arr=list(map(int, input().split()))
+func(arr)
