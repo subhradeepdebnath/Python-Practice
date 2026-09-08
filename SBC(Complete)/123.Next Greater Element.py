8d8dac8c62227c03ed93e33f12e685617ef9eee4
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-08 11:21:50 +0530

    DSA

---
+++ SBC(Complete)/123.Next Greater Element.py
@@ -0,0 +1,12 @@
+def func(arr):
+    a=[]
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[j]>arr[i]:
+                a.append(arr[j])
+                break
+        if len(a)==i:
+            a.append(-1)
+    print(*a)
+arr=list(map(int, input().split()))
+func(arr)
