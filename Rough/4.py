72cc2d4d9c954a07b237cf534e31ca9041c9ca3a
Subhradeep Debnath <subhradeepdebnath2004@gmail.com>
2026-09-13 15:21:51 +0530

    DSA

---
+++ Rough/4.py
@@ -0,0 +1,11 @@
+# Floyd's Triangle
+def func(n):
+    num=1
+    for i in range(1, n+1):
+        for j in range(i):
+            print(num, end=" ")
+        
+            num+=1
+        print()
+n=int(input())
+func(n)
